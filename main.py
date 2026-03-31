250aa8270317ee4c62eeb6284bd2d36168ba9246
theshivrajpatil <theshivrajpatil@gmail.com>
2026-03-31 22:37:51 +0530

    USing model Mistral

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
 
-model = OllamaLLM(model="llama3.2")
+model = OllamaLLM(model="mistral")
 
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 db = Chroma(persist_directory="./chrome_langchain_db", embedding_function=embeddings)
